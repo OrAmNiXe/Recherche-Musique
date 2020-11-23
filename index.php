c86317d4815592ad2635e7dcf5c030739041d66f
OrAmNiXe <62951218+OrAmNiXe@users.noreply.github.com>
2020-11-23 10:10:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<title>Music application</title>
+		<title>Music application1</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 		<link rel="stylesheet" href="./css/style.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
